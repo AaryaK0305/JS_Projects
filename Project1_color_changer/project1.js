1d4dd707313fa6fb0a566a2a6883e155960df4bb
AaryaK0305 <aaryakamatkar03@gmail.com>
2026-03-27 00:14:30 +0530

    project 2 added

---
+++ Project1_color_changer/project1.js
@@ -0,0 +1,24 @@
+const buttons =document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach((button)=>
+{
+    button.addEventListener('click',(e)=>
+    {
+        switch(e.target.id)
+        {
+            case "yellow":
+                body.style.backgroundColor = e.target.id;
+                break;
+            case "grey":
+                body.style.backgroundColor = e.target.id;
+                break;
+            case "red":
+                body.style.backgroundColor = e.target.id;
+                break;
+            case "blue":
+                body.style.backgroundColor = e.target.id;
+                break;     
+        }
+    });
+});
